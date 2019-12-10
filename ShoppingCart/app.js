0beeb5ac234bb43c369ae4c19a6954edfcfc166a
john <wayaway.jtm@gmail.com>
2019-12-09 19:38:40 -0500

    added currency input handling

---
+++ ShoppingCart/app.js
@@ -0,0 +1,35 @@
+
+
+//
+// Taken from here: https://stackoverflow.com/questions/24163889/html5-input-for-money-currency
+//
+
+var currencyInput = document.querySelector('input[type="currency"]');
+var currency = 'USD'; // https://www.currency-iso.org/dam/downloads/lists/list_one.xml
+
+currencyInput.addEventListener('focus', onFocus);
+currencyInput.addEventListener('blur', onBlur);
+
+function localStringToNumber( s ){
+    return Number(String(s).replace(/[^0-9.-]+/g,""));
+}
+
+function onFocus(e){
+  var value = e.target.value;
+  e.target.value = value ? localStringToNumber(value) : '';
+}
+
+function onBlur(e){
+  var value = e.target.value;
+
+  const options = {
+      maximumFractionDigits : 2,
+      currency              : currency,
+      style                 : "currency",
+      currencyDisplay       : "symbol"
+  }
+  
+  e.target.value = value 
+    ? localStringToNumber(value).toLocaleString(undefined, options)
+    : ''
+}
